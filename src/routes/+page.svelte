8d637d200341653f4a6b2f8fe6f434f125800605
Mapagmataas <t.s.anashin@inp.nsk.su>
2025-04-17 19:30:47 +0700

    Hot fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,28 +25,28 @@
 
 <div class="card m-4 size-16"></div>
 
-<div class="bg-bg my-2 flex flex-row items-center justify-center space-x-4 p-8">
+<div class="bg-bg my-2 flex flex-col items-center justify-center space-x-4 p-8">
 	<div class="text-muted-fg">text</div>
 	<div class="text-fg">text</div>
 	<a href="/zxc" class="link">link</a>
 	<a href="/" class="link">link</a>
-	<button class="primary">Primary</button>
-	<button class="secondary">Secondary</button>
-	<button class="notify">Notify</button>
-	<button class="success">Success</button>
-	<button class="warning">Warning</button>
-	<button class="error">Error</button>
+	<button class="primary m-2">Primary</button>
+	<button class="secondary m-2">Secondary</button>
+	<button class="notify m-2">Notify</button>
+	<button class="success m-2">Success</button>
+	<button class="warning m-2">Warning</button>
+	<button class="error m-2">Error</button>
 </div>
 
-<div class="bg-muted my-2 flex flex-row items-center justify-center space-x-4 p-8">
+<div class="bg-muted my-2 flex flex-col items-center justify-center space-x-4 p-8">
 	<div class="text-muted-fg">text</div>
 	<div class="text-fg">text</div>
 	<a href="/zxc" class="link">link</a>
 	<a href="/" class="link">link</a>
-	<button class="primary">Primary</button>
-	<button class="secondary">Secondary</button>
-	<button class="notify">Notify</button>
-	<button class="success">Success</button>
-	<button class="warning">Warning</button>
-	<button class="error">Error</button>
+	<button class="primary m-2">Primary</button>
+	<button class="secondary m-2">Secondary</button>
+	<button class="notify m-2">Notify</button>
+	<button class="success m-2">Success</button>
+	<button class="warning m-2">Warning</button>
+	<button class="error m-2">Error</button>
 </div>
